b6d778c92a771e22ab11742898c9529162afbb80
andrewyee12 <ayee@villanova.edu>
2018-02-22 12:36:57 +0100

    modified scene 1

--- Assets/Scripts/JumpPowerUp.cs
+++ Assets/Scripts/JumpPowerUp.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JumpPowerUp : MonoBehaviour {
+
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if(other.tag == "Player") {
