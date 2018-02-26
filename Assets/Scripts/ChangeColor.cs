4fcf4b4d144f4ca16bb5762b440f942826814774
Tristan Stone <tjs264@cornell.edu>
2018-02-26 21:27:41 +0100

    finished another level

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            player.GetComponent<SpriteRenderer>().color = Color.red;
+            Debug.Log("COLORCHANGE");
+        }
+    }
+}
