1bc251ff56a8616f88d194454d714779dced9bfc
Tristan Stone <tjs264@cornell.edu>
2018-03-01 00:31:05 +0100

    fixed shooting bug

---
+++ Assets/Scripts/Proj.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Proj : MonoBehaviour
+{
+    public float speed = 2;
+    public float lifeTime = 3;
+    public Vector2 direction = new Vector2(1, 0);
+    public bool flip = false;
+
+
+    void Start()
+    {
+
+        Debug.Log("start");
+        StartCoroutine(KillAfterSeconds(lifeTime));
+        direction.Normalize();
+        if (flip)
+        {
+            Debug.Log("SDFSDFSDFSDFSDFSDF");
+            GetComponent<SpriteRenderer>().flipX = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, 0f);
+        }
+        else if (!flip)
+        {
+            Debug.Log("LLLLLLLLLLL");
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0f);
+        }
+
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+
+    }
+
+    IEnumerator KillAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/S.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class S : MonoBehaviour
+{
+
+    private bool hasGun = true;
+    public GameObject bullet;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            if (hasGun)
+            {
+                Vector3 pos = transform.position;
+                pos.y -= 0.1f;
+                if (bullet.GetComponent<Proj>().flip == true)
+                {
+                    pos.x -= 0.5f;
+                }
+                else {
+                    pos.x += 0.5f;
+                }
+                Instantiate(bullet, pos, Quaternion.identity);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            GetComponent<SpriteRenderer>().flipX = true;
+            bullet.GetComponent<Proj>().flip = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            GetComponent<SpriteRenderer>().flipX = false;
+            bullet.GetComponent<Proj>().flip = false;
+        }
+    }
+}
